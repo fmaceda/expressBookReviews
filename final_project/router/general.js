f476f80859f0cdbf124c53a4541fa1abb7fbd3ee
Fernando Maceda <ferna.mace.34@gmail.com>
2026-06-08 20:14:30 -0400

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,12 +12,15 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
+  // Send JSON response with formatted books data
   res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  
+  // Retrieve the isbn parameter from the request URL and send the corresponding book details
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn],null,4));
 });
   
 // Get book details based on author
