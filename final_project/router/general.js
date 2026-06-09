65eb4ea75b846be5facc7cb52fe1d969272ce9f2
Fernando Maceda <ferna.mace.34@gmail.com>
2026-06-08 20:16:56 -0400

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,8 +25,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the author parameter from the request URL and send the corresponding book details
+  const author = req.params.author;
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+  res.send(JSON.stringify(filteredBooks,null,4));
 });
 
 // Get all books based on title
